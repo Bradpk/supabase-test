ec085769fe005126f3d7c9186779dfcd32774bf1
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-04-26 13:32:01 -0400

    g

--- supa/src/app/page.js
+++ supa/src/app/page.js
@@ -2,16 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Test from "../components/test";
 import supabase from "@/api/supabase";
+import Signup from "@/components/signup";
 
 export default function Home() {
   console.log(supabase)
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing
-        </p>
         <Test />
+        <Signup />
         </div>
     </main>
   );
