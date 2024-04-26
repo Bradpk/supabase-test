0d9cf31c13d483e2abc4d28d056c7c920afdff96
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-04-26 13:36:24 -0400

    g

--- supa/src/components/signup.js
+++ supa/src/components/signup.js
@@ -1,7 +1,16 @@
 export default function Signup(){
     return(
         <>
-        <form></form>
+        <form>
+            <label>Email:</label>
+            <input></input><br></br>
+            <label>Password:</label>
+            <input></input><br></br>
+            <label>Confirm Password:</label>
+            <input></input>
+        </form>
+        <button>Sign Up</button>
+
         </>
     )
 }
